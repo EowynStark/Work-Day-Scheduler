7bbe98bf110b97edba06e79dd6a9bf462a0eb33c
EowynStark <andwellm89@gmail.com>
2023-11-14 16:46:34 -0600

    adding event listener and local set storage code

--- Assets/script.js
+++ Assets/script.js
@@ -26,21 +26,24 @@ function timeblockUpdate() {
     }
 }
 
-$(function (){
-});
+// adds event listener to the save button
+// saves input to local storage by the hour as a string 
+document.addEventListener('click', function saveScheduleItem(event) {
+    if (event.target.classList.contains('saveBtn')) {
+        var scheduleContainer = event.target.closest('.time-block');
+        var scheduleItem = scheduleContainer.querySelector('.description').value;
+        var scheduleId = scheduleContainer.id;
+        localStorage.setItem(scheduleId, JSON.stringify({scheduleItem}));
+    }
+})
 
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
  
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
 
-
+    $(function (){
+    });
     
  
 // runs display of time
